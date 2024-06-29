a682a6e3cc1e317ee8e346f4ba418072edfdb9e2
AlvinNathey <alvin.nathey@strathmore.edu>
2024-06-29 18:42:52 +0300

    fixed admin side when deleting a user

--- deleteusers.php
+++ deleteusers.php
@@ -1,18 +1,35 @@
 <?php
 require_once("connection.php");
 
-if(isset ($_GET["id"])){
+if(isset($_GET["id"])){
     $user_id = $_GET["id"];
 
-    require_once("connection.php");
+    // Start transaction
+    mysqli_begin_transaction($conn);
 
-    $sql = "DELETE FROM users WHERE user_id ='$user_id' ";
+    try {
+        // Delete from search_history first to avoid foreign key constraint issues
+        $sql1 = "DELETE FROM search_history WHERE user_id ='$user_id'";
+        if (!mysqli_query($conn, $sql1)) {
+            throw new Exception("Error deleting from search_history: " . mysqli_error($conn));
+        }
 
-    if (mysqli_query($conn, $sql)) {
+        // Delete from users
+        $sql2 = "DELETE FROM users WHERE user_id ='$user_id'";
+        if (!mysqli_query($conn, $sql2)) {
+            throw new Exception("Error deleting from users: " . mysqli_error($conn));
+        }
+
+        // Commit transaction
+        mysqli_commit($conn);
+
         header('location: adminloggedin.php');
         exit;
-    } else {
-        echo "Error deleting record: " . mysqli_error($conn);
+    } catch (Exception $e) {
+        // Rollback transaction
+        mysqli_rollback($conn);
+
+        echo $e->getMessage();
     }
 } else {
     echo "User ID not provided.";
